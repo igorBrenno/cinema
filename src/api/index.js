2659703d9d3910c6005ec940a98a9a0122d3d0a7
Igor Brenno Dantas <54316405+igorBrenno@users.noreply.github.com>
2024-12-05 17:41:23 +0000

    feat: Update

--- src/api/index.js
+++ src/api/index.js
@@ -8,7 +8,6 @@ import axios from "axios";
 export const api = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     params: {
-        api_key: "e67068cf824a77155eebbb9210999861",
-        language: "PT-BR",
+        api_key: "e67068cf824a77155eebbb9210999861"
     }
 });
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,13 @@ import './MoviesComponent.css';
 
 const MoviesComponent = () => {
     const [movies, setMovies] = useState([]);
+    const [searchItem, setSearchItem] = useState('')
 
-    const fetchMovies = async () => {
-        try {
+    const handleInputChange = (e) => { 
+        e.preventDefault();
+
+        const searchTerm = e.target.value;
+        setSearchItem(e.target[0].valueS
             const respostaMovie = await api.get('/movie/top_rated', {
                 params: {
                     page: 1,
@@ -17,14 +21,37 @@ const MoviesComponent = () => {
             console.error("Erro ao buscar filmes:", error);
         }
     };
-
+    const search = async () => {
+        try {
+            const respostaMovie = await api.get(`/search/movie?query=${searchItem}`, {
+                params: {
+                    page: 1,
+                }
+            });
+            setMovies(respostaMovie.data.results);
+        } catch (error) {
+            console.error("Erro ao buscar filmes:", error);
+        }
+    };
     useEffect(() => {
-        fetchMovies();
-    }, []);
+        if(!searchItem){
+
+            fetchMovies();
+        } else {
+            search()
+            
+        }
+    }, [searchItem]);
 
     return (
         <div className="movies-container">
+            <div id='divseila'>
             <h1 className="movies-title">Filmes Populares</h1>
+            <form onSubmit={(e)=> handleInputChange(e)} className='seach'>
+            <input type="text" placeholder='seach' id='seach'/>
+            <input type="submit" />
+            </form>
+            </div>
             <div className="movies-grid">
                 {movies.map((movie) => (
                     <div key={movie.id} className="movie-card">
